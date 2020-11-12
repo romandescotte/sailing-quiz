059cc6efa2d11725474cc030328390b21667835d
sk8mumy <romandescotte@gmail.com>
2020-11-12 17:41:55 -0300

    Cambia logíca de mezcla de respuestas
    Se modificó el código para que al generar el examen el motor mezcle entre respuestas que tengan que ver con la pregunta. De esta manera se evita que el multiplechoice contenga respuestas que por lógica no son las correctas. Por ejemplo: "Que es un buque?" Respuestas posibles "La baluma", "Vela mayor", "El puntal" y "Flotador dotado de propulsión propia y destinado a navegar con seguridad en cualquer superficie líquida". En este caso el usuario podría descartar sin siquiera tener conocimiento de cosa las primeras tres respuestas y quedarse con la cuarta respuesta.

--- js/create_html_todojunto_resp_correcta2.js
+++ js/create_html_todojunto_resp_correcta2.js
@@ -1,107 +1,38 @@
 'use strict'
+//Hay que modificar la estructura de objetos para que al mezclar preguntas con respuestas no figuren 
+//respuestas que no tegan que ver con la pregunta y que no sea facil de responer.
+//Para ello vamos a generar un objeto pregunta que contenga las respuestas posibles. 
 
-      
-
-//Constructor de clase de objeto pregunta
-
-function Pregunta(id_pregunta, seccion, id_seccion, texto, id_numero) {
-	this.id_pregunta = id_pregunta;
-	this.seccion = seccion;
-	this.id_seccion = id_seccion;
-	this.texto = texto;		
-	this.id_numero = id_numero;
-
-}
-
-//Constructor de clase de objeto respuesta
-
-function Respuesta(id_pregunta, seccion, id_seccion, texto, id_numero) {
-	this.id_pregunta = id_pregunta;
-	this.seccion = seccion;
-	this.id_seccion = id_seccion;
-	this.texto = texto;		
-	this.id_numero = id_numero;
-	
-}
-
-//Creando objetos pregunta en base al constructor
-
-let pregunta1 = new Pregunta("P1C", "Expresiones marineras", "C", "¿A qué se conoce como trasluchada?", 1);
-let pregunta2 = new Pregunta("P2C", "Expresiones marineras", "C", "¿A qué se conoce como navegar en ceñida?", 2);
-let pregunta3 = new Pregunta("P3C", "Expresiones marineras", "C", "¿A qué se conoce como bordejear?", 3);
-let pregunta4 = new Pregunta("P4C", "X", "X", "qwe", 4);
-let pregunta5 = new Pregunta("P5C", "X", "X", "asd", 5);
-let pregunta6 = new Pregunta("P6C", "X", "X", "zxc", 6);
-let pregunta7 = new Pregunta("P7C", "X", "X", "rty", 7);
-let pregunta8 = new Pregunta("P8C", "X", "X", "fgh", 8);
-let pregunta9 = new Pregunta("P9C", "X", "X", "vbn", 9);
-let pregunta10 = new Pregunta("P10C", "X", "X", "uio", 10);
-let pregunta11 = new Pregunta("P11C", "X", "X", "jkl", 11);
+function PreguntasyRespuestas(numeroPregunta, textoPregunta, numeroRespuestaCorrecta, textoRespuestaCorrecta, respuestasPosibles) {
+    
+    this.numeroPregunta = numeroPregunta;
+    this.textoPregunta = textoPregunta;
+    this.numeroRespuestaCorrecta = numeroRespuestaCorrecta;
+    this.textoRespuestaCorrecta = textoRespuestaCorrecta;    
+    this.respuestasPosibles = respuestasPosibles;
+}      
+    
 
 
-//Agregar la pregunta creado al contenedor:
+let item1 = new PreguntasyRespuestas(1, "asd?", 1, "asd", ["qwe","rty","uio","fgh","jkl", "zxc", "vbn"]);
+let item2 = new PreguntasyRespuestas(2, "fgh?", 2, "fgh", ["qwe","rty","uio","asd","jkl", "zxc", "vbn"]);
+let item3 = new PreguntasyRespuestas(3, "jkl?", 3, "jkl", ["qwe","rty","uio","asd","fgh", "zxc", "vbn"]);
+let item4 = new PreguntasyRespuestas(4, "zxc?", 4, "zxc", ["qwe","rty","uio","asd","jkl", "fgh", "vbn"]);
+let item5 = new PreguntasyRespuestas(5, "vbn?", 5, "vbn", ["qwe","rty","uio","asd","jkl", "zxc", "fgh"]);
+let item6 = new PreguntasyRespuestas(6, "rty?", 6, "rty", ["qwe","fgh","uio","asd","jkl", "zxc", "vbn"]);
+let item7 = new PreguntasyRespuestas(7, "uio?", 7, "uio", ["qwe","rty","fgh","asd","jkl", "zxc", "vbn"]);
 
-let contenedorPreguntas = [
-                    pregunta1, 
-                    pregunta2, 
-                    pregunta3, 
-                    pregunta4, 
-                    pregunta5, 
-                    pregunta6, 
-                    pregunta7, 
-                    pregunta8, 
-                    pregunta9,
-                    pregunta10,
-                    pregunta11
-                    ];
+let contenedorItems = [item1,item2,item3,item4,item5,item6,item7];
 
 
-//Creando objetos respuesta en base al constructor
-
-let respuesta1 = new Respuesta("R1C", "Expresiones marineras", "C", "Al golpe de la botavara al virar en redondo ó según costumbres a la virada en redondo propiamente dicha", 1);
-let respuesta2 = new Respuesta("R2C", "Expresiones marineras", "C", "Navegar con el ángulo más chico posible entre el viento y la línea de crujía", 2);
-let respuesta3 = new Respuesta("R3C", "Expresiones marineras", "C", "Sucesión de virados por avante que se realizan con el fin de alcanzar un punto que se encuentra sobre le eje del viento", 3);
-let respuesta4 = new Respuesta("R4C", "X", "X", "qwe", 4);
-let respuesta5 = new Respuesta("R5C", "X", "X", "asd", 5);
-let respuesta6 = new Respuesta("R6C", "X", "X", "zxc", 6);
-let respuesta7 = new Respuesta("R7C", "X", "X", "rty", 7);
-let respuesta8 = new Respuesta("R8C", "X", "X", "fgh", 8);
-let respuesta9 = new Respuesta("R9C", "X", "X", "vbn", 9);
-let respuesta10 = new Respuesta("R10C", "X", "X", "uio", 10);
-let respuesta11 = new Respuesta("R10C", "X", "X", "jkl", 11);
-
-//Agregar la respuesta creada al contenedor:
-
-let contenedorRespuestas = [
-                            respuesta1,
-                            respuesta2,
-                            respuesta3,
-                            respuesta4,
-                            respuesta5,
-                            respuesta6,
-                            respuesta7,
-                            respuesta8,
-                            respuesta9,
-                            respuesta10,
-                            respuesta11
-                            ];
-
-
-
-
-// Elige numeros al azar sin repetir dada la cantidad deseada, de existir 
-// el parametro numeroPreexistente, lo mete adentro del array cajaDeNumeros 
 function cubilete(cantidadDeNumeros, numeroPreexistente, min, max) {        
     
     let cajaDeNumeros = [];
     cajaDeNumeros.push(numeroPreexistente);    
     if(cajaDeNumeros[0] == undefined ) {
-
         cajaDeNumeros.pop();
-    }             
-
-    while(cajaDeNumeros.length < cantidadDeNumeros) {  
-
+    } 
+        while(cajaDeNumeros.length < cantidadDeNumeros) {
         function generaNumeroAleatorio (min, max) {
                 min = Math.ceil(min);
                 max = Math.floor(max);
@@ -110,17 +41,15 @@ function cubilete(cantidadDeNumeros, numeroPreexistente, min, max) {
         let numeroAleatorio = generaNumeroAleatorio(min, max);
 
         function chequeaSiExiste(elementosDelArray) {
-
             return elementosDelArray == numeroAleatorio;
         }
 
         let resultadoChequeaSiExiste = cajaDeNumeros.some(chequeaSiExiste);        
         if(resultadoChequeaSiExiste == false) {
-
             cajaDeNumeros.push(numeroAleatorio);            
         }  
     }    
-//    console.log("Cubilete:" + cajaDeNumeros);
+    //console.log("Cubilete:" + cajaDeNumeros);
     return cajaDeNumeros;
 }   
 
@@ -130,55 +59,47 @@ function ItemsPrevio(pregunta, respuestas) {
     this.respuestas = [];
 }
 
-let listaPreviaItemsDePregunta = [];
+let contenedorItemsPrevio = [];
 
-function seleccionDePreguntas(cantidadDePreguntas, min, max) {
-	
-    let preguntasSeleccionadas = cubilete(cantidadDePreguntas, undefined, min, max);
-    
-    //console.log("Preguntas seleccionadas: "+preguntasSeleccionadas); 
+let cantidadPreguntasContenedorItems = contenedorItems.length;
+
+function seleccionadorPreguntas(cantidadDePreguntas, min, max) {
+
+    let preguntasSeleccionadas = cubilete(cantidadDePreguntas, undefined, min, max);    
     for(let i = 0; i < preguntasSeleccionadas.length; i++) {
 
-        listaPreviaItemsDePregunta.push(new ItemsPrevio(preguntasSeleccionadas[i]));
-        
+        contenedorItemsPrevio.push(new ItemsPrevio(preguntasSeleccionadas[i]));        
     }    
     return preguntasSeleccionadas; 
 }
 
+function seleccionadorRespuestas(cantidadDeRespuestas, min, max) {
 
+   for(let j = 0; j < contenedorItemsPrevio.length ; j++) {
+        let nroPregunta = contenedorItemsPrevio[j].pregunta;
+        max = contenedorItems[nroPregunta].respuestasPosibles.length-1;
+        let nuevasRespuestas = cubilete(cantidadDeRespuestas, contenedorItemsPrevio[j].pregunta, min, max);
+        for(let i = 0 ; i < nuevasRespuestas.length; i++) {
 
-function seleccionDeRespuestas(cantidadDeRespuestas, min, max) {
-
-   for(let j = 0; j < listaPreviaItemsDePregunta.length ; j++) {
-
-		let nuevasRespuestas = cubilete(cantidadDeRespuestas, listaPreviaItemsDePregunta[j].pregunta, min, max);
-
-		for(let i = 0 ; i < nuevasRespuestas.length; i++) {
-			listaPreviaItemsDePregunta[j].respuestas.push(nuevasRespuestas[i]);
-		}
-   }	         
+            contenedorItemsPrevio[j].respuestas.push(nuevasRespuestas[i]);
+        }
+   }         
 }
-
-
-
-
 
 function ItemsExamen(pregunta, respuestas) {
-	this.pregunta = pregunta,
-	this.respuestas = []
+    this.pregunta = pregunta,
+    this.respuestas = []
 }
 
-let listaItemsExamen = [];
+let contenedorItemsExamen = [];
 
 function mezclaRespuestas(min, max) {
 
-	for(let i = 0; i < listaPreviaItemsDePregunta.length; i++) {
+    for(let i = 0; i < contenedorItemsPrevio.length; i++) {
 
-		listaItemsExamen.push(new ItemsExamen(listaPreviaItemsDePregunta[i].pregunta));		
-
-	    let cajaMezcladora = [];
-    
-        while(cajaMezcladora.length < listaPreviaItemsDePregunta[i].respuestas.length) {
+        contenedorItemsExamen.push(new ItemsExamen(contenedorItemsPrevio[i].pregunta));     
+        let cajaMezcladora = [];    
+        while(cajaMezcladora.length < contenedorItemsPrevio[i].respuestas.length) {
 
             function generaNumeroAleatorio (min, max) {
                 min = Math.ceil(min);
@@ -186,37 +107,31 @@ function mezclaRespuestas(min, max) {
                 return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
             }
             let numeroAleatorio = generaNumeroAleatorio(min, max);
+            function chequeaSiExiste(elementosDelArray) {
 
-			function chequeaSiExiste(elementosDelArray) {
+                return elementosDelArray == numeroAleatorio;
+            }
 
-				return elementosDelArray == numeroAleatorio;
-			}
+            let resultadoChequeaSiExiste = cajaMezcladora.some(chequeaSiExiste);            
+            if(resultadoChequeaSiExiste == false) {
 
-			let resultadoChequeaSiExiste = cajaMezcladora.some(chequeaSiExiste);        
-			
-			if(resultadoChequeaSiExiste == false) {
                 let j = 0;
-				cajaMezcladora.push(numeroAleatorio);
-
-				listaItemsExamen[i].respuestas.push(listaPreviaItemsDePregunta[i].respuestas[numeroAleatorio]);  
-				j++;          
-			}  
+                cajaMezcladora.push(numeroAleatorio);
+                contenedorItemsExamen[i].respuestas.push(contenedorItemsPrevio[i].respuestas[numeroAleatorio]);  
+                j++;          
+            }  
        }
    }   
-   return listaItemsExamen;
-
+   return contenedorItemsExamen;
 }
 
-
-function muestraPreguntas() {
+function muestraItemsExamen() {
     
     let contadorId2 = 0;
-
-    for(let i = 0 ; i < listaItemsExamen.length ; i++) {
+    for(let i = 0 ; i < contenedorItemsExamen.length ; i++) {
 
         let contadorId1 = i + 1;
-
-        let texto_pregunta = `${contadorId1}) ${contenedorPreguntas[listaItemsExamen[i].pregunta].texto}`;
+        let texto_pregunta = `${contadorId1}) ${contenedorItems[contenedorItemsExamen[i].pregunta].textoPregunta}`;
 
         //Agarra formulario existente
         let $formulario = document.getElementById('form_preguntas');
@@ -244,16 +159,21 @@ function muestraPreguntas() {
         //Se adjunta el nodo al elemento.
         $h3.appendChild($nodo_texto_pregunta);
 
-        // Crea array con preguntas asignadas
-        //preguntasSeleccionadas.push(contenedorPreguntas[j].texto);  
-
         // Respuestas:
         
-        for(let j = 0 ; j < listaItemsExamen[0].respuestas.length; j++) {
+        for(let j = 0 ; j < contenedorItemsExamen[0].respuestas.length; j++) {
             
-            contadorId2++;        	
-            
-            let texto_respuestas = contenedorRespuestas[listaItemsExamen[i].respuestas[j]].texto;           
+            contadorId2++;          
+            let nroRespuesta = contenedorItemsExamen[i].respuestas[j];
+            let nroPregunta = contenedorItemsExamen[i].pregunta;
+            var texto_respuestas;                       
+            if(nroRespuesta == nroPregunta) {
+
+                texto_respuestas = `${contenedorItems[nroPregunta].textoRespuestaCorrecta}`;
+            } else {
+
+                texto_respuestas = `${contenedorItems[nroPregunta].respuestasPosibles[nroRespuesta]}`;
+            }                     
 
             //Seleccionar el elemento existente
             let $ul = document.querySelector(`ul[id="ul_pregunta_${contadorId1}"]`);
@@ -273,7 +193,8 @@ function muestraPreguntas() {
             let input_id = `input_respuesta_${contadorId2}`;
             let input_name = `name_input_${contadorId1}`;
             let label_for = input_id;
-            let label_id = `id_label_${contadorId2}`;                         
+            let label_id = `id_label_${contadorId2}`; 
+            let input_value = texto_respuestas;                        
 
             $crea_li.setAttribute("id", li_id);
             $crea_inputRadio.setAttribute("id", input_id);
@@ -281,9 +202,10 @@ function muestraPreguntas() {
             $crea_inputRadio.setAttribute("name", input_name);
             $crea_label.setAttribute("for", label_for);
             $crea_label.setAttribute("id", label_id);
+            $crea_inputRadio.setAttribute("value", input_value);
 
 
-            // Asignacion de la respuesta al elemento HTML de la lista:
+            //Asignacion de la respuesta al elemento HTML de la lista:
             //Se selecciona el elemento variable                
             let $label = document.querySelector(`ul[id="${ul_id}"] label[id="${label_id}"]`);
 
@@ -291,29 +213,26 @@ function muestraPreguntas() {
             let $nodo_respuesta = document.createTextNode(texto_respuestas);
 
             //Se adjunta el nodo al elemento.
-            $label.appendChild($nodo_respuesta);
+            $label.appendChild($nodo_respuesta);            
+        }
+    }
+}
+//Solo cambiar cantidadPreguntas y cantidadRespuestas
 
-            
-            
-            }
+function generarExamen(cantidadPreguntas, cantidadRespuestas, numeroIteracionMaxPreguntas) {
 
+    if(cantidadPreguntas <= cantidadPreguntasContenedorItems) {
+
+        seleccionadorPreguntas(cantidadPreguntas, 0, numeroIteracionMaxPreguntas);
+        seleccionadorRespuestas(cantidadRespuestas, 0);
+        console.log(contenedorItemsPrevio);
+        mezclaRespuestas(0,cantidadRespuestas-1);
+        console.log(contenedorItemsExamen);
+        muestraItemsExamen();       
+    } else {    
+
+        console.log("El número requerido de preguntas a mostrar es mayor a la cantidad de preguntas disponibles");
     }
 }
 
-
-// cantidadPreguntas: la cantidad de preguntas que contendrá el examen
-// cantidadRespuestas: la cantidad de respuestas que contendrá el examen
-// numeroIteracionMaxPreguntas: el motor mezclara hasta este numero de pregunta. No puede superar la longitud del array contenedorPreguntas
-// numeroIteracionMaxRespuestas: el motor mezclara hasta este numero de pregunta. No puede superar la longitud del array contenedorRespuestas
-
-
-function generarExamen(cantidadPreguntas, cantidadRespuestas, numeroIteracionMaxPreguntas, numeroIteracionMaxRespuestas){
-    seleccionDePreguntas(cantidadPreguntas, 0, numeroIteracionMaxPreguntas);
-    seleccionDeRespuestas(cantidadRespuestas, 0, numeroIteracionMaxRespuestas);
-    console.log(listaPreviaItemsDePregunta);
-    mezclaRespuestas(0,cantidadRespuestas-1);
-    console.log(listaItemsExamen);
-    muestraPreguntas();
-}
-
-generarExamen(10, 4, 10, 10);
+generarExamen(7, 4, cantidadPreguntasContenedorItems-1);

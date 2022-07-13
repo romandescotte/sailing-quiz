604216b16bf310c13060c54d812f02798894e66b
Roman <romandescotte@gmail.com>
2022-07-13 16:23:29 -0300

    vuelve a colocar 20 preguntas

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { generarExamen } from "./generarExamen.js"
 import { corregirExamen } from "./corregirExamen.js"
 
-let cantidadPreguntas = 2; //96 maximo
+let cantidadPreguntas = 20; //96 maximo
 let cantidadRespuestas = 4; //si pongo mas de 4 a veces se traba el loop y aveces entra en el if que comprueba si hay respuestas posibles
 
 generarExamen(cantidadPreguntas, cantidadRespuestas);
